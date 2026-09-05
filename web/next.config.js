5a1ba9fa03fbfa8a7e9cbf8311c108709471e858
semulabangkit1985-ctrl <semulabangkit1985@gmail.com>
2026-09-05 18:19:51 +0800

    Create next.config.js

---
+++ web/next.config.js
@@ -0,0 +1,32 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  experimental: {
+    typedRoutes: false,
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
